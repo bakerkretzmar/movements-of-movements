4c75a4edaf9f5cde60d2fffa5483ae1900bfb14a
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2020-03-15 14:43:58 -0400

    Update project structure and assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,29 +1,20 @@
-let mix = require('laravel-mix');
+const mix = require('laravel-mix')
+require('laravel-mix-purgecss')
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix.disableNotifications()
 
 mix.js('resources/js/site.js', 'public/js')
-   .sass('resources/sass/site.scss', 'public/css');
+    .postCss('resources/css/site.css', 'public/css', [
+        require('tailwindcss'),
+    ])
+    // .version()
 
-
-/*
- |--------------------------------------------------------------------------
- | Statamic Control Panel Assets
- |--------------------------------------------------------------------------
- |
- | Feel free to add your own JS or CSS to the Statamic Control Panel.
- | https://statamic.dev/extending/control-panel#adding-css-and-js-assets
- |
- */
+if (mix.inProduction()) {
+    mix.sourceMaps().purgeCss()
+}
 
 // mix.js('resources/js/cp.js', 'public/vendor/app/js')
-//    .sass('resources/sass/cp.scss', 'public/vendor/app/css');
+//     .postCss('resources/css/cp.css', 'public/vendor/app/css', [
+//         require('tailwindcss'),
+//     ])
+//     .version()
