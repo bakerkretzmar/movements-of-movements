ec62a089d42baa93e86c45c2bb353a85dddabf40
Jacob Baker-Kretzmar <jacobtbk@gmail.com>
2020-06-19 19:19:18 -0400

    Update default email setup to Postmark

--- config/services.php
+++ config/services.php
@@ -14,29 +14,8 @@ return [
     |
     */
 
-    'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
-    ],
-
-    'ses' => [
-        'key' => env('SES_KEY'),
-        'secret' => env('SES_SECRET'),
-        'region' => 'us-east-1',
-    ],
-
-    'sparkpost' => [
-        'secret' => env('SPARKPOST_SECRET'),
-    ],
-
-    'stripe' => [
-        'model' => App\User::class,
-        'key' => env('STRIPE_KEY'),
-        'secret' => env('STRIPE_SECRET'),
-    ],
-
-    'paypal' => [
-        'client_id' => env('PAYPAL_CLIENT_ID'),
+    'postmark' => [
+        'token' => env('POSTMARK_TOKEN'),
     ],
 
 ];
